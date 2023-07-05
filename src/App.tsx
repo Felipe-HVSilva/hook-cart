88cad3ddd33b2db1186a19cae00c0004e510ee1c
FelipeSilva <felype.hen@hotmail.com>
2023-07-04 21:10:49 -0300

    context Card create

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
+import { CartProvider } from './context/useCart'
 import { Home } from './pages/Home'
 import { GlobalStyle } from './styles/global'
 
 function App() {
   return (
-    <>
+    <CartProvider>
       <GlobalStyle />
       <Home />
-    </>
+    </CartProvider>
   )
 }
 
---
+++ src/context/useCart.tsx
@@ -0,0 +1,34 @@
+import { ReactNode, createContext, useState } from 'react'
+
+interface CartProps {
+  id: number
+  price: string
+  title: string
+  image: string
+}
+
+interface CartContextProps {
+  addItemCart: (newItem: CartProps) => void
+}
+
+interface CartProviderProps {
+  children: ReactNode
+}
+
+export const CartContext = createContext({} as CartContextProps)
+
+export function CartProvider({ children }: CartProviderProps) {
+  const [cart, setCart] = useState<CartProps[]>([])
+
+  function addItemCart(newItem: CartProps) {
+    setCart([...cart, newItem])
+  }
+
+  console.log(cart)
+
+  return (
+    <CartContext.Provider value={{ addItemCart }}>
+      {children}
+    </CartContext.Provider>
+  )
+}
